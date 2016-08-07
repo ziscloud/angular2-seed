fdba834ddfc91b01122270fb036a671ce5d5d761
Tony Wang <ziscloud@gmail.com>
2016-08-07 23:15:37 +0800

    added typings and updated gulpfile task

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,67 @@
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
-  entry: {
-    'app': './src/app.ts',
-    'vendor': './src/vendor.ts'
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: "bundle.js"
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
-  ],
 
-  resolve: {
-    extensions: ['', '.ts', '.js']
+// Webpack Config
+var webpackConfig = {
+  entry: {
+    'polyfills': './src/polyfills.browser.ts',
+    'vendor': './src/vendor.browser.ts',
+    'main': './src/main.browser.ts',
   },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "dist/"
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.CommonsChunkPlugin({name: ['main', 'vendor', 'polyfills'], minChunks: Infinity}),
+  ],
 
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts-loader' },
-    ],
-    noParse: [ /angular2\/bundles\/.+/ ]
+      // .ts files for TypeScript
+      {test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader']},
+      {test: /\.css$/, loaders: ['to-string-loader', 'css-loader']},
+      {test: /\.html$/, loader: 'raw-loader'}
+    ]
+  }
+
+};
+
+
+// Our Webpack Defaults
+var defaultConfig = {
+  devtool: 'cheap-module-source-map',
+  cache: true,
+  debug: true,
+  output: {
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  resolve: {
+    root: [path.join(__dirname, 'src')],
+    extensions: ['', '.ts', '.js']
   },
 
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
+    watchOptions: {aggregateTimeout: 300, poll: 1000}
+  },
+
+  node: {
+    global: 1,
+    crypto: 'empty',
+    module: 0,
+    Buffer: 0,
+    clearImmediate: 0,
+    setImmediate: 0
   }
 };
+
+var webpackMerge = require('webpack-merge');
+module.exports = webpackMerge(defaultConfig, webpackConfig);
